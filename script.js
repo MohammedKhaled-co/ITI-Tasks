adec960ec307a0a74192df30fadca6e5de1d0d95
MohammedKhaled-co <mohammedkhaled.contact@gmail.com>
2025-09-17 05:14:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// Q1 in HTML
+
+
+
+// Q2
+// let x = 5;
+// x=x.toString()
+// console.log(x,typeof(x))
+
+// Q3
+// let x = window.prompt("Enter First Number!")
+// let y = window.prompt("Enter Second Number!")
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(parseInt(x),parseInt(y)))
+
+// Q4
+// let x = window.prompt("Enter First Number!")
+// if(x>0){
+//     console.log("This Number is Positive!")
+// }
+// else if(x<0){
+//     console.log("This Number is Negative!")
+// }
+// else{
+//     console.log("This Number is Zero!")
+// }
+
+// Q5
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// Q6
+// for(let i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// Q7
+// let color = window.prompt("Enter Your Favorite Color")
+// switch(color){
+//     case "red":
+//         console.log("red");
+//         break;
+//     case "green":
+//         console.log("green")
+//         break;
+//     default:
+//         console.log("other")
+//         break;  
+// }
+
+// Q8
+// let x = 5%0;
+// console.log(x);
+// let y = null;
+// console.log(y)
+// z = 
+// console.log(typeof(z))
+
+// Q9
+//  let x = window.prompt("Enter String");
+ 
+// function parse (x){
+//     x=parseInt(x);
+//     console.log(x,typeof(x));
+   
+//  }
+// parse(x);
+
+// Q10
+// const obj={
+//     name: "Mohammed",
+//     id:29890,
+//     age: 18
+// }
+// console.log(obj.gender) //undefined
+
+// Q11
+// function multi(x,y){
+
+// }
+
+// console.log(multi(4,7)) //undefined
+
+// Q12
+// let x = window.prompt("Enter Number!")
+// if(x%2==0){
+//     console.log("number is even")
+//     if(x>0)
+//     {
+//         console.log("and positive")
+//     }   
+// }
+
+// Q13
+// let temp=0;
+// for(let i=1;i<=100;i++){
+//     temp+=i
+// }
+// console.log(temp);
+
+
+
+
+
+
